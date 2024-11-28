0b8c3d5b766af1a212e8cf117749871d830773c3
Boyarin90 <lomoti1990@gmail.com>
2024-11-28 13:19:33 +0900

    Add files via upload

---
+++ lesson04.py
@@ -0,0 +1,7 @@
+example='boyarin'
+print(example[0],example[-1],example[4:],example[::-1],example[1:8:2])
+print(example[0])
+print(example[-1])
+print(example[4:])
+print(example[::-1])
+print(example[1:8:2])
